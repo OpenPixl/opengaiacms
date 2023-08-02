600414222c2484fe429753832210f74e9be3f91c
Corwin40 <xavier.burke@openpixl.fr>
2023-08-02 18:04:34 +0200

    Initialisation du projet

---
+++ src/Controller/Webapp/PublicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Webapp;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicController extends AbstractController
+{
+    #[Route('/', name: 'app_webapp_public_index')]
+    public function index(): Response
+    {
+        return $this->render('webapp/public/index.html.twig', [
+            'controller_name' => 'PublicController',
+        ]);
+    }
+}
