5e7d57bf65d3e94fcf07a1f10c3b129d7d2ec0c1
Corwin40 <xavier.burke@openpixl.fr>
2023-12-25 08:48:34 +0100

    DEV -ADMIN | Modal Block

---
+++ src/Controller/Webapp/BlockController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller\Webapp;
+
+use App\Entity\Webapp\Block;
+use App\Form\Webapp\BlockType;
+use App\Repository\Webapp\BlockRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/webapp/block')]
+class BlockController extends AbstractController
+{
+    #[Route('/', name: 'app_webapp_block_index', methods: ['GET'])]
+    public function index(BlockRepository $blockRepository): Response
+    {
+        return $this->render('webapp/block/index.html.twig', [
+            'blocks' => $blockRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/listadmin', name: 'app_webapp_block_listadmin', methods: ['GET'])]
+    public function listAdmin(BlockRepository $blockRepository): Response
+    {
+        return $this->render('webapp/block/list.html.twig', [
+            'blocks' => $blockRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_webapp_block_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $block = new Block();
+        $form = $this->createForm(BlockType::class, $block);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($block);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_webapp_block_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('webapp/block/new.html.twig', [
+            'block' => $block,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_webapp_block_show', methods: ['GET'])]
+    public function show(Block $block): Response
+    {
+        return $this->render('webapp/block/show.html.twig', [
+            'block' => $block,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_webapp_block_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Block $block, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(BlockType::class, $block);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_webapp_block_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('webapp/block/edit.html.twig', [
+            'block' => $block,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_webapp_block_delete', methods: ['POST'])]
+    public function delete(Request $request, Block $block, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$block->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($block);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_webapp_block_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/Webapp/BlockType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form\Webapp;
+
+use App\Entity\Webapp\Block;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlockType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Block::class,
+        ]);
+    }
+}
